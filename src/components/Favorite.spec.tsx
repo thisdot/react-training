0b3e2f4c814587b0d70dd4f648ded7dcbe2d9a58
Maarten Bicknese <maarten.bicknese@thisdot.co>
2023-01-26 10:16:28 +0100

    test: add async and store tests

---
+++ src/components/Favorite.spec.tsx
@@ -0,0 +1,22 @@
+import FavoriteButton from "./FavoriteButton";
+import FavoriteCount from "./FavoriteCount";
+import { fireEvent, render, screen } from "@testing-library/react";
+
+describe("favorites store", () => {
+  it("starts with 0 favorites", () => {
+    render(<FavoriteCount />);
+    expect(screen.getByText("0")).toBeInTheDocument();
+  });
+  it("increments when adding a profile", () => {
+    render(
+      <>
+        <FavoriteCount />
+        <FavoriteButton id="dummy" />
+      </>
+    );
+
+    fireEvent.click(screen.getByRole("button"));
+
+    expect(screen.getByText("1")).toBeInTheDocument();
+  });
+});
