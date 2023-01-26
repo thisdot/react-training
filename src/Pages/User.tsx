146122159fc0b10881c07a60b7a786d5ad75aa84
Maarten Bicknese <maarten.bicknese@thisdot.co>
2023-01-26 10:34:48 +0100

    fix: use params for user profile page

--- src/Pages/User.tsx
+++ src/Pages/User.tsx
@@ -1,21 +1,24 @@
 import UserProfile from "../components/UserProfile";
 import useProfile from "../hooks/useProfile";
-import {IProfile} from "../types/users";
+import { IProfile } from "../types/users";
+import { useParams } from "react-router-dom";
 
-const isProfile = (profile: any): profile is IProfile => Object.keys(profile).length === 0
-type UserProps = {
-    userId: string
-}
-const User = ({ userId }: UserProps) => {
-    const { isLoading, isError, data } = useProfile(userId);
-    if (isLoading) {
-        return <p>Loading profile...</p>
-    }
-    if(isError || !isProfile(data)) {
-        return <p>An error occurred!</p>
-    }
+const isProfile = (profile: any): profile is IProfile =>
+  Object.keys(profile).length === 0;
+type UserParams = {
+  userId: string;
+};
+const User = () => {
+  const { userId }: UserParams = useParams();
+  const { isLoading, isError, data } = useProfile(userId);
+  if (isLoading) {
+    return <p>Loading profile...</p>;
+  }
+  if (isError || !isProfile(data)) {
+    return <p>An error occurred!</p>;
+  }
 
-    return <UserProfile profile={data} />
-}
+  return <UserProfile profile={data} />;
+};
 
 export default User;
